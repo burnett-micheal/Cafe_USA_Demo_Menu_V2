a28d3658b1c51a17a9772778aa72c609e01597cb
burnett-micheal <burnett.micheal.business@gmail.com>
2022-06-05 06:18:16 -0500

    replaced category dropdowns with slider using Swiper library

--- src/Components/Displays/Phone/Phone.jsx
+++ src/Components/Displays/Phone/Phone.jsx
@@ -2,55 +2,11 @@ import { useState } from "react";
 import "../../../Styles/phone.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import menuData from "../../../Data/menuData";
-function Category(props) {
-
-  return (
-    <div className={`category ${props.isActive ? "active" : "inactive"}`}>
-      <div
-        className={`title container ${props.isActive ? "active" : "inactive"} flexX`}
-      >
-        <div className="arrow container centerX">
-          <FontAwesomeIcon style={{ width: "11vw", height: "11vw" }} icon={props.isActive ? faAngleUp : faAngleDown} />
-        </div>
-        <div
-          className={`centerText pointerHover text`}
-        >{props.title}</div>
-        <div className="arrow container centerX">
-          <FontAwesomeIcon style={{ width: "11vw", height: "11vw" }} icon={props.isActive ? faAngleUp : faAngleDown} />
-        </div>
-      </div>
-      {
-        props.isActive && (
-          <div className="cat-description">
-            {props.description}
-          </div>
-        )
-      }
-
-      {props.isActive && props.items.map(
-        (itemData, index) => (
-          <div key={index} className={`item container`}>
-            <div className="item name">{itemData.name}</div>
-            <div className="item description">{itemData.description}</div>
-            <div className="item price">{itemData.price}</div>
-          </div>
-        )
-      )}
-    </div>
-  );
-}
 
 function Phone() {
-  const [categoriesToggle, setCategoriesToggle] = useState(menuData.categories.map(x => false));
-
-  const toggleCategory = (e) => {
-    const catIndex = e.currentTarget.id;
-    const newToggles = [...categoriesToggle];
-    newToggles[catIndex] = !categoriesToggle[catIndex]
-    setCategoriesToggle(newToggles);
-  }
-
   return (
     <div className="phone-display noTextSelect">
       <div className="banner" alt="..">
@@ -61,27 +17,34 @@ function Phone() {
       <div className="header-description">
         We're proud to offer a 15% discount to Military and First Responders
       </div>
-
-      <div>
-        {
-          menuData.categories.map(
-            (catData, index) => (
-              <div
-                key={index}
-                id={index}
-                onClick={toggleCategory}
-              >
-                <Category
-                  title={catData.name}
-                  description={catData.description}
-                  isActive={categoriesToggle[index]}
-                  items={catData.items}
-                />
-              </div>
-            )
-          )
-        }
-      </div>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        style={{ paddingTop: "4vw", paddingBottom: "4vw" }}
+        autoHeight
+        loop
+      >
+        {menuData.categories.map((category, activeIndex) => (
+          <SwiperSlide
+            key={activeIndex}
+          >
+            <div style={{ textAlign: "center", fontSize: "11vw" }}>{category.name}</div>
+            <div>
+              {
+                menuData.categories[activeIndex].items.map(
+                  (itemData, index) => (
+                    <div key={index} className={`item container`}>
+                      <div className="item name">{itemData.name}</div>
+                      <div className="item description">{itemData.description}</div>
+                      <div className="item price">{itemData.price}</div>
+                    </div>
+                  )
+                )
+              }
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
